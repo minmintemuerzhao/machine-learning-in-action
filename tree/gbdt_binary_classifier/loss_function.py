1c62d4b9aae854d1d5e4df120283fd2d5571a9d3
minmintemuerzhao <tdzm1351@163.com>
2021-05-11 10:19:03 +0800

    增加gbdt二分类

--- tree/gbdt_binary_classifier/loss_function.py
+++ tree/gbdt_binary_classifier/loss_function.py
@@ -1,4 +1,4 @@
-
+import math
 import abc
 
 
@@ -25,16 +25,21 @@ class LossFunction(metaclass=abc.ABCMeta):
         """计算训练损失"""
 
 
-class SquaresError(LossFunction):
+class BinomialDeviance(LossFunction):
 
     def initialize_f_0(self, data):
-        data['f_0'] = data['label'].mean()
-        return data['label'].mean()
+        pos = data['label'].sum()
+        neg = data.shape[0] - pos
+        # 此处log是以e为底，也就是ln
+        f_0 = math.log(pos / neg)
+        data['f_0'] = f_0
+        return f_0
 
     def calculate_residual(self, data, iter):
+        # calculate negative gradient
         res_name = 'res_' + str(iter)
         f_prev_name = 'f_' + str(iter - 1)
-        data[res_name] = data['label'] - data[f_prev_name]
+        data[res_name] = data['label'] - 1 / (1 + data[f_prev_name].apply(lambda x: math.exp(-x)))
 
     def update_f_m(self, data, trees, iter, learning_rate, logger):
         f_prev_name = 'f_' + str(iter - 1)
@@ -46,8 +51,16 @@ class SquaresError(LossFunction):
         self.get_train_loss(data['label'], data[f_m_name], iter, logger)
 
     def update_leaf_values(self, targets, y):
-        return targets.mean()
+        numerator = targets.sum()
+        if numerator == 0:
+            return 0.0
+        denominator = ((y - targets) * (1 - y + targets)).sum()
+        if abs(denominator) < 1e-150:
+            return 0.0
+        else:
+            return numerator / denominator
 
     def get_train_loss(self, y, f, iter, logger):
-        loss = ((y - f) ** 2).mean()
-        logger.info(('第%d棵树: mse_loss:%.4f' % (iter, loss)))
+        loss = -2.0 * ((y * f) - f.apply(lambda x: math.exp(1 + x))).mean()
+
+        logger.info(('第%d棵树: log-likelihood:%.4f' % (iter, loss)))
